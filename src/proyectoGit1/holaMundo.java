ff1374a56cc0e90184df0797a9b1fa22954b9527
jj <jjuanuceda@gmail.com>
2020-08-13 12:52:32 +0200

    Commit 3

--- src/proyectoGit1/holaMundo.java
+++ src/proyectoGit1/holaMundo.java
@@ -3,8 +3,8 @@ package proyectoGit1;
 public class holaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hola mundo");
+		//
 	}
 
 }
